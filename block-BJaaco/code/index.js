79d386be2337ce7aa431c5ab10536687f6f97655
sohail shaik <sohailshaik765@gmail.com>
2021-04-21 11:37:30 +0530

    Assignment level-2 block-3

--- block-BJaaco/code/index.js
+++ block-BJaaco/code/index.js
@@ -3,10 +3,23 @@ let numbers = [6, 8, 10, 12, 43, 56, 98];
 let userIds = [1230, 234, 1278, 984, 763, 900];
 
 // 1. Add all the values of numbers and userIds array into the new newly created array named `collection`
+let collection = [6, 8, 10, 12, 43, 56, 98, 1230, 234, 1278, 984, 763, 900]
 
 // 2. Add all the even numbers from both arrays numbers and userIds into a newly created array named `evenCollection`
+let evenCollection = []
+for(let i = 0 ; i < collection ; i++){
+  if(collection[i] % 2 === 0){
+    evenCollection.push(collection[i])
+  }
+}
 
 // 3. Add all the odd numbers from both arrays numbers and userIds into a newly created array named `oddCollection`
+let oddCollection = []
+for(let i = 0 ; i < collection ; i++){
+  if(collection[i] % 2 !== 0){
+    oddCollection.push(collection[i])
+  }
+}
 
 /*
   @param means parameter
@@ -24,8 +37,12 @@ let userIds = [1230, 234, 1278, 984, 763, 900];
     times(5); // ['test', 'test', 'test', 'test', 'test']
 */
 
-function times() {
+function times(num, str) {
   // Your code
+  let empty_arr = []
+  for(let i = str ; i < num ; i++){
+    empty_arr.push(i)
+  }
 }
 
 // Uncomment the code below and test the output
@@ -48,8 +65,11 @@ function times() {
     revert(['Ryan', 'John', 'Bran']); //['Bran', 'John', 'Ryan']
 */
 
-function revert() {
+function revert(arr) {
   // your code
+  for(let i = arr ; i < arr.length - 1 ; i--){
+    console.log(arr[i])
+  }
 }
 
 // Uncomment the code below and test the output
@@ -70,8 +90,18 @@ function revert() {
     clear(['Ryan', null, 0,  'John', 'Bran']); //['Bran', 'John', 'Ryan']
 */
 
-function clear() {
+function clear(arr) {
   // your code
+  for(let i = 0 ; i < arr.length - 1 ; i--){
+    if(arr[i] == false || arr[i] == undefined || arr[i] == "" || arr[i] == 0 || arr[i] == null){
+      console.log(delete arr[i])
+
+    }else{
+      return arr[i]
+    }
+  }
+  
+
 }
 
 // Uncomment the code below and test the output
@@ -93,8 +123,13 @@ function clear() {
     arrayToObj(['Ryan', 'John']); // {0: 'Ryan', 1: 'John'}
 */
 
-function arrayToObj() {
+function arrayToObj(arr) {
   // your code
+  for(let i = 0 ; i < arr.length ; i++){
+    if(arr[i] == []){
+      return arr[i]
+    }
+  }
 }
 
 // Uncomment the code below and test the output
